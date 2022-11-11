1ea4396b30a8a271620b6411adb1d61269eb0993
Dewflio <leonard.puskac@gmail.com>
2022-11-11 20:40:44 +0100

    merged indexer and searcher into one lucene controller class

---
+++ src/vinf_lucene_controller.py
@@ -0,0 +1,9 @@
+import lucene
+
+class VINF_Lucene_Controller:
+    def __init__(self, index_directory):
+        self.index_directory = index_directory
+
+    def create_index(index_path, infilename, index_outfilename):
+        pass
+        
